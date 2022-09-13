2b44f6e1d5111431db63d08e555605d459e9737a
willianAD <willian.afonso013@gmail.com>
2022-09-13 17:21:49 -0300

    Requisito 2

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -2,14 +2,12 @@ const fetchProducts = async (param) => {
   try {
     const url = `https://api.mercadolibre.com/sites/MLB/search?q=${param}`;
     const response = await fetch(url);
-    const data = await response.json();
-    return data;
+    const objeto = await response.json();
+    return objeto;
   } catch (error) {
     throw new Error('You must provide an url');
   }
 };
-
-console.log(fetchProducts());
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// Esse tipo de comentário que estão antes de todas as funções são chamados de JSdoc,a
-// experimente passar o mouse sobre o nome das funções e verá que elas possuem descrições! 
+// Esse tipo de comentário que estão antes de todas as funções são chamados de JSdoc,
+// experimente passar o mouse sobre o nome das funções e verá que elas possuem descrições!
 
 // const { fetchProducts, fetchProducts } = require("./helpers/fetchProducts");
 
@@ -10,12 +10,12 @@
  * @param {string} imageSource - URL da imagem.
  * @returns {Element} Elemento de imagem do produto.
  */
-// const createProductImageElement = (imageSource) => {
-//   const img = document.createElement('img');
-//   img.className = 'item__image';
-//   img.src = imageSource;
-//   return img;
-// };
+const createProductImageElement = (imageSource) => {
+  const img = document.createElement('img');
+  img.className = 'item__image';
+  img.src = imageSource;
+  return img;
+};
 
 /**
  * Função responsável por criar e retornar qualquer elemento.
@@ -24,12 +24,12 @@
  * @param {string} innerText - Texto do elemento.
  * @returns {Element} Elemento criado.
  */
-// const createCustomElement = (element, className, innerText) => {
-//   const e = document.createElement(element);
-//   e.className = className;
-//   e.innerText = innerText;
-//   return e;
-// };
+const createCustomElement = (element, className, innerText) => {
+  const e = document.createElement(element);
+  e.className = className;
+  e.innerText = innerText;
+  return e;
+};
 
 /**
  * Função responsável por criar e retornar o elemento do produto.
@@ -39,24 +39,27 @@
  * @param {string} product.thumbnail - URL da imagem do produto.
  * @returns {Element} Elemento de produto.
  */
-// const createProductItemElement = ({ id, title, thumbnail }) => {
-//   const section = document.createElement('section');
-//   section.className = 'item';
+const createProductItemElement = ({ id, title, thumbnail }) => {
+  const section = document.createElement('section');
+  section.className = 'item';
 
-//   section.appendChild(createCustomElement('span', 'item_id', id));
-//   section.appendChild(createCustomElement('span', 'item__title', title));
-//   section.appendChild(createProductImageElement(thumbnail));
-//   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
+  section.appendChild(createCustomElement('span', 'item_id', id));
+  section.appendChild(createCustomElement('span', 'item__title', title));
+  section.appendChild(createProductImageElement(thumbnail));
+  section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
 
-//   return section;
-// };
+  return section;
+};
 
 /**
  * Função que recupera o ID do produto passado como parâmetro.
  * @param {Element} product - Elemento do produto.
  * @returns {string} ID do produto.
  */
-// const getIdFromProductItem = (product) => product.querySelector('span.id').innerText;
+
+// const getIdFromProductItem = (product) => product.querySelector('span.id').innerText; ESTE ESTÁ ERRADO!!!
+
+// const getIdFromProductItem = (product) => product.querySelector('span.item_id').innerText;
 
 /**
  * Função responsável por criar e retornar um item do carrinho.
@@ -74,15 +77,22 @@
 //   return li;
 // };
 
-window.onload = () => { };
+const objetoResults = async (param) => {
+  const itemsClass = document.getElementsByClassName('items')[0];
+  const getProd = await fetchProducts(param);
+  getProd.results.forEach((prod) => { 
+    const obj = { 
+      id: prod.id,
+      title: prod.title,
+      thumbnail: prod.thumbnail, 
+    };
+    const insertProdct = createProductItemElement(obj);
+    itemsClass.appendChild(insertProdct);
+  });
+};
 
-// const dataObj = async () => {
-//   const destruction = await fetchProducts('computador');
-//   const { result } = destruction;
-//   for (let index = 0; index < result.length; index++) {
-//     const { id, title, thumbnail} = result[index];
-//     itemsClass.appendChild(createProductItemElement({
-//       id, title, thumbnail,
-//     }));
-//   }
-// };
+window.onload = async () => {
+    objetoResults('computador');
+    // const teste = await fetchProducts('computador');
+    // console.log(teste.results);
+};
